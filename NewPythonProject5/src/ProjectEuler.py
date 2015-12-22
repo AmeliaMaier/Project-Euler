351836b3bc181877dd3c4a44b7eb5efd6c3b7fdb
AmeliaMaier <Amelia.Maier.Jobsearch@gmail.com>
2015-12-22 17:20:45 -0600

    Fixing Problem 1 to match prompt
    reread problem 1 - realized asking for something completely different

--- NewPythonProject5/src/ProjectEuler.py
+++ NewPythonProject5/src/ProjectEuler.py
@@ -4,39 +4,39 @@
 import sys
 import os
 
-main_menu()
-
 def main_menu():
     options_list = ('Problem 1: Find the sum of all multiples of x or y below z.', 'Problem 2: Find the sum of the even-valued terms in the Fibonacci sequesnce whose values do not exceed x.', 'Problem 3: Find the largest prime factor of x.')
     print ("Main Menu")
     for name in options_list: 
         print(name)
     main_menu_input = input("Which problem would you like solved? ")
-    if main_menu_input == '1':
+    if main_menu_input == 1:
         problem_one()
-    elif main_menu_input == '2':
+    elif main_menu_input == 2:
         problem_two()
-    elif main_menu_input == '3':
+    elif main_menu_input == 3:
+        problem_three()
     else:
-    print("That is not a valid option.")
+        print("That is not a valid option.")
 
 def problem_one():
-    multiple_x, multiple_y, count, sum = 0
+    possible_multiple = 1 
+    sum = 0
     x = input('Enter a value for X: ')
     y = input('Enter a value for Y: ')
     z = input('Enter a value for Z: ')
-    while multiple_x < z:
-        count= count + 1
-        multiple_x = count*x
-        if multiple_x <= z:
-            list_of_multiples.append(multiple_x)
-            sum = sum + multiple_x
-    count = 0
-    while multiple_y < z:
-        count= count + 1
-        multiple_y = count*y
-        if multiple_y <= z & multiple_y not in list_of_multiples:
-            list_of_multiples.append(multiple_y)
-            sum = sum + multiple_y
-    print('The sum of all the mutlipes of ', x, ' or ', y, ' below ', z, ' is ', sum)
+    while possible_multiple < z:
+        if possible_multiple % x == 0:
+            sum = sum + possible_multiple
+        elif possible_multiple % y == 0:
+            sum = sum + possible_multiple
+        possible_multiple = possible_multiple + 1
+    print 'The sum of all the mutlipes of ', x, ' or ', y, ' below ', z, ' is ', sum
     
+def problem_two():
+    print ('under construction')
+def problem_three():
+    print ('under construction')
+    
+main_menu()
+
--- NewPythonProject5/src/setup.py
+++ NewPythonProject5/src/setup.py
@@ -1,5 +1,5 @@
 __author__ = "Lia"
-__date__ = "$Dec 22, 2015 4:42:51 PM$"
+__date__ = "$Dec 22, 2015 5:20:03 PM$"
 
 from setuptools import setup, find_packages
 
