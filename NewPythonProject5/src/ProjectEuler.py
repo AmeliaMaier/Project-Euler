420fbd3f45de2439a18439f55eb657a3bdb539a0
AmeliaMaier <Amelia.Maier.Jobsearch@gmail.com>
2015-12-22 09:07:40 -0600

    Created New Project
    practice based on Project Euler

---
+++ NewPythonProject5/src/ProjectEuler.py
@@ -0,0 +1,8 @@
+# Work done based on Project Euler to practice recreational math & Python
+# Open Source Project
+
+__author__ = "Lia"
+__date__ = "$Dec 22, 2015 9:06:01 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
