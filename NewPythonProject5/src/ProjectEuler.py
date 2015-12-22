97c23813824c30369605cfac1a59214b9650cd44
AmeliaMaier <Amelia.Maier.Jobsearch@gmail.com>
2015-12-22 16:03:24 -0600

    creating menu interface

--- NewPythonProject5/src/ProjectEuler.py
+++ NewPythonProject5/src/ProjectEuler.py
@@ -1,8 +1,13 @@
 # Work done based on Project Euler to practice recreational math & Python
 # Open Source Project
 
-__author__ = "Lia"
-__date__ = "$Dec 22, 2015 9:06:01 AM$"
+import sys
+import os
 
-if __name__ == "__main__":
-    print "Hello World"
+options_list = ('Problem 1: Find the sum of all multiples of x or y below z.', 'Problem 2: Find the sum of the even-valued terms in the Fibonacci sequesnce whose values do not exceed x.', 'Problem 3: Find the largest prime factor of x.')
+print ("Main Menu")
+for name in options_list: 
+    print(name)
+
+
+
