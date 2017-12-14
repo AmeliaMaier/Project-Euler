3386d13ef9ba3e2041aae99fb43fd640c3ccfa13
AmeliaMaier <amelia.maier.jikili@gmail.com>
2017-12-14 11:46:25 -0700

    Completed Problem 3

--- Problem3_LargestPrimeFactor.py
+++ Problem3_LargestPrimeFactor.py
@@ -1,4 +1,19 @@
+import math
+
 primeNumberList = []
-max = 600851475143
+current = 600851475143
+
 #need to determine how to find prime factors
 #print largets prime factor of max
+while current%2 ==0:
+    primeNumberList.append(2)
+    current = current/2
+
+for possible in range(3, int(math.sqrt(current))+1, 2):
+    while current % possible == 0:
+        primeNumberList.append(possible)
+        current = current/possible
+if current > 2:
+    primeNumberList.append(current)
+
+print (max(primeNumberList))
