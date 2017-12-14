71b68a4bc7c73031042b66e33587421e5936898a
AmeliaMaier <amelia.maier.jikili@gmail.com>
2017-12-14 08:26:18 -0700

    Completed Problem 1&2. Working on Problem 3

---
+++ Problem2_EvenFibonacciNumbers.py
@@ -0,0 +1,12 @@
+max = 400000000
+fibonacci_num_low = 1
+fibonacci_num_high = 2
+fibonacci_sum = 0
+sum = 2
+while(fibonacci_num_high <= max):
+    fibonacci_sum = fibonacci_num_low + fibonacci_num_high
+    fibonacci_num_low = fibonacci_num_high
+    fibonacci_num_high = fibonacci_sum
+    if(fibonacci_num_high % 2 == 0):
+        sum = sum + fibonacci_num_high
+print (sum)
---
+++ Problem3_LargestPrimeFactor.py
@@ -0,0 +1,4 @@
+primeNumberList = []
+max = 600851475143
+#need to determine how to find prime factors
+#print largets prime factor of max
