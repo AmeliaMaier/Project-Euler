7490981ab8783e6296cdb01883f21aab71a68b13
AmeliaMaier <amelia.maier.jikili@gmail.com>
2017-12-14 18:36:03 -0700

    Bug fix Problem 4, redesign due to wrong answer

--- Problem4_LargestPalindromeProduct.py
+++ Problem4_LargestPalindromeProduct.py
@@ -1,6 +1,5 @@
-import math
+'''
 possible_palindrome = 999*999
-smallest_possible_factor = 100
 answer_found = False
 #continues until the correct palindrome is found
 while(answer_found == False and possible_palindrome >= 100*100):
@@ -10,9 +9,20 @@ while(answer_found == False and possible_palindrome >= 100*100):
     #check if it has 2 whole number multipiers with 3 digits
     x = 999
     while(x>=100):
-        if(possible_palindrome%x == 0):
+        if(possible_palindrome%x == 0 and possible_palindrome/x >=100):
             print (possible_palindrome)
             answer_found = True
             break
         x = x - 1
     possible_palindrome = possible_palindrome - 1
+'''
+
+min=100
+max=999
+max_palindrome = 0
+for x in range(min,max + 1):
+    for y in range(x + 1, max + 1): # avoid duplicates
+        prod = x*y
+        if prod > max_palindrome and str(prod)==(str(prod)[::-1]):
+            max_palindrome = prod
+print (max_palindrome)
